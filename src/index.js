6151ff0d4a49773c45242c8f7eaa6bb8fa2f613c
Jake Ascher <ascherj@gmail.com>
2019-11-03 16:57:37 -0500

    Refactor Board render method to use two for loops

--- src/index.js
+++ src/index.js
@@ -16,30 +16,29 @@ class Board extends React.Component {
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        key={i}
       />
     );
   }
 
   render() {
+    let boardRows = [];
+
+    for (let row = 0; row < 3; row++) {
+      const boardRow = [];
+      for (let col = 0; col < 3; col++) {
+        boardRow.push(this.renderSquare(col + (row * 3)));
+      }
+      boardRows.push(<div className="board-row" key={row}>{boardRow}</div>);
+    }
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {boardRows}
       </div>
     );
+
+
   }
 }
 
